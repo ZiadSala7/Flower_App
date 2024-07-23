cef910a758686651e7f00f08b564e57fa2a1bcc7
Ziad Salah <zslah2800@gmail.com>
2024-07-23 22:01:05 +0300

    custom action appbar

--- lib/features/home/presentation/views/widgets/custom_my_cart_app_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_my_cart_app_bar.dart
@@ -1,8 +1,7 @@
 import 'package:flower_app/constants.dart';
 import 'package:flower_app/core/utils/styles.dart';
-import 'package:flower_app/features/home/presentation/view%20models/products_manager_cubit/products_cubit.dart';
+import 'package:flower_app/core/widgets/custom_action_app_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 AppBar customMyCartAppBar(BuildContext context) {
   return AppBar(
@@ -14,33 +13,6 @@ AppBar customMyCartAppBar(BuildContext context) {
         color: Colors.white,
       ),
     ),
-    actions: [
-      Row(
-        children: [
-          Stack(
-            children: [
-              CircleAvatar(
-                radius: 10,
-                backgroundColor: Colors.amber,
-                child: Text(
-                  BlocProvider.of<ProductsCubit>(context).totalPrice.toString(),
-                  style: const TextStyle(fontSize: 12),
-                ),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.add_shopping_cart),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 8),
-            child: Text(
-              BlocProvider.of<ProductsCubit>(context).totalPrice.toString(),
-            ),
-          ),
-        ],
-      ),
-    ],
+    actions: const [CustomActionAppBar()],
   );
 }
